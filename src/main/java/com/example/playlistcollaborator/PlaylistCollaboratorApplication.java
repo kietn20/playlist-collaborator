cf1dadaca4cb1fa7e5885675ab64b2c9741d6b64
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-04-24 23:01:08 -0700

    setting up

---
+++ src/main/java/com/example/playlistcollaborator/PlaylistCollaboratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.playlistcollaborator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistCollaboratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistCollaboratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/playlistcollaborator/PlaylistCollaboratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playlistcollaborator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistCollaboratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
