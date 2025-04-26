b34c2cfbdb06d04ff9d1415bd6175f210f903a32
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-04-25 22:51:08 -0700

    generated comments

--- src/main/java/com/example/playlistcollaborator/exception/RoomNotFoundException.java
+++ src/main/java/com/example/playlistcollaborator/exception/RoomNotFoundException.java
@@ -1,13 +1,18 @@
+// File: src/main/java/com/example/playlistcollaborator/exception/RoomNotFoundException.java
+// Purpose: Custom exception for when a requested Room cannot be found.
+// Location: src/main/java/com/example/playlistcollaborator/exception/
+
 package com.example.playlistcollaborator.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+// This annotation maps the exception to an HTTP 404 Not Found status code automatically
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class RoomNotFoundException extends RuntimeException{
+public class RoomNotFoundException extends RuntimeException {
 
     public RoomNotFoundException(String publicId) {
-        super("Room not found with public id: " + publicId);
+        super("Room not found with publicId: " + publicId);
     }
 
     public RoomNotFoundException(String message, Throwable cause) {
