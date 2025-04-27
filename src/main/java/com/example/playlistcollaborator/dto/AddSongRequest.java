cf5cddb0db98ecc2d291ad4de5966bcafb24ada9
Kiet Nguyen <kietnguyen3698@gmail.com>
2025-04-27 14:13:32 -0700

    generated comments for addSongRequest dto

--- src/main/java/com/example/playlistcollaborator/dto/AddSongRequest.java
+++ src/main/java/com/example/playlistcollaborator/dto/AddSongRequest.java
@@ -1,13 +1,19 @@
+// File: src/main/java/com/example/playlistcollaborator/dto/AddSongRequest.java
+// Purpose: DTO representing a client's request to add a song via WebSocket.
+// Location: src/main/java/com/example/playlistcollaborator/dto/
+
 package com.example.playlistcollaborator.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddSongRequest {
+    // We don't need roomPublicId here if the client sends to a room-specific destination like /app/room/{roomId}/addSong
     private String title;
     private String artist;
+    // Could add 'requester' field later if user accounts are added
 }
