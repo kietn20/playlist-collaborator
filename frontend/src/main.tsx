52f5c1a2909b6608e0b30b2f0160a99c51643d3d
kietn20 <kietnguyen3698@gmail.com>
2025-05-04 18:56:20 -0700

    refactor main.tsx to improve import structure and maintain consistency

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// File: src/main.tsx
+// Purpose: Entry point of the React application.
+// Location: src/
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
