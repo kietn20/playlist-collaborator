5728f66890347a030a429046ab6a5722f6fa41c5
kietn20 <kietnguyen3698@gmail.com>
2025-06-30 17:57:58 -0700

    feat: create NextSongRequestDto for handling next song requests

---
+++ backend/src/main/java/com/example/playlistcollaborator/dto/NextSongRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.playlistcollaborator.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class NextSongRequestDto {
+  private String username;
+}
